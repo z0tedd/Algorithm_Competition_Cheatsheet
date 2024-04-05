1169ed5fa7ee721e66bbf7e3cc383486cc666559
z0tedd <z0tedd@gmail.com>
2024-04-05 20:03:36 +0000

    update

---
+++ TEMPLATE.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i,n) for(size_t i = 1;i<n;i++)
+void solve();
+int main(){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  solve();
+  return 0;
+}
+void solve(){
+
+
+}
+
+//cout.tie(0); Для магических компиляторов.
+//String transformation to lower case, using cctype.
+//transform(a.begin(), a.end(), a.begin(),
+//    [](unsigned char c){ return std::tolower(c); });
+//
+//
+//
+//
