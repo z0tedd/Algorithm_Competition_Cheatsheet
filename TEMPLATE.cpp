17289ebcd320faa4b2663d2f4c3a16557f4c43a5
z0tedd <z0tedd@gmail.com>
2024-04-15 17:47:52 +0000

    Added some macros

--- TEMPLATE.cpp
+++ TEMPLATE.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define range(i,n) for(size_t i = 1;i<n;i++)
+#define range(i,n) for(size_t i = 0;i<n;i++)
+#define rpt(i,a,b) for(size_t i = a;i<b;i++)
+#define all(v) begin(v),end(v)
+typedef long long lg;
+typedef vector<int> iv;
+typedef vector<lg> lv;
 void solve();
 int main(){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
+  cin.exceptions(cin.failbit); //Throws an exception, if you have an input error. Variable overflow will summon RTE, not WA;
+  //int t; cin >> t; while(t--)
   solve();
   return 0;
 }
 void solve(){
-
 
 }
 
